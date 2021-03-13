8f005e757f97aad7b1560085198242f9677eb13c
AlexValder <angelaarhangelska@gmail.com>
2021-03-13 18:06:50 +0200

    Code style

--- DBLab2/DBLab2/Common/Printer.cs
+++ DBLab2/DBLab2/Common/Printer.cs
@@ -2,31 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 
-namespace DBLab2.Common
-{
-    public static class Printer
-    {
-        private enum MessageTypes
-        {
+namespace DBLab2.Common {
+    public static class Printer {
+        private enum MessageTypes {
             Info,
             Success,
             Error,
         }
 
         private static readonly ImmutableDictionary<MessageTypes, ConsoleColor> Foregrounds =
-            new Dictionary<MessageTypes, ConsoleColor>
-            {
-                [MessageTypes.Info] = ConsoleColor.White,
+            new Dictionary<MessageTypes, ConsoleColor> {
+                [MessageTypes.Info]    = ConsoleColor.White,
                 [MessageTypes.Success] = ConsoleColor.Green,
-                [MessageTypes.Error] = ConsoleColor.Red,
+                [MessageTypes.Error]   = ConsoleColor.Red,
             }.ToImmutableDictionary();
         
         private static readonly ImmutableDictionary<MessageTypes, ConsoleColor> Backgrounds =
-            new Dictionary<MessageTypes, ConsoleColor>
-            {
-                [MessageTypes.Info] = ConsoleColor.Black,
+            new Dictionary<MessageTypes, ConsoleColor> {
+                [MessageTypes.Info]    = ConsoleColor.Black,
                 [MessageTypes.Success] = ConsoleColor.Black,
-                [MessageTypes.Error] = ConsoleColor.Black,
+                [MessageTypes.Error]   = ConsoleColor.Black,
             }.ToImmutableDictionary();
 
         public static void Info(string template, params object[] args)
@@ -38,24 +33,20 @@ namespace DBLab2.Common
         public static void Error(string template, params object[] args)
             => Print(MessageTypes.Error, template, args);
 
-        public static void Error(Exception ex, string template)
-        {
+        public static void Error(Exception ex, string template) {
             var temp = string.Concat(template, ex.Message, ex.StackTrace);
             Print(MessageTypes.Error, temp);
         }
 
-        private static void Print(MessageTypes type, string template, params object[] args)
-        {
+        private static void Print(MessageTypes type, string template, params object[] args) {
             var foreTmp = Console.ForegroundColor;
             var backTmp = Console.BackgroundColor;
-            try
-            {
+            try {
                 Console.ForegroundColor = Foregrounds[type];
                 Console.BackgroundColor = Backgrounds[type];
                 Console.WriteLine(template, args);
             }
-            finally
-            {
+            finally {
                 Console.ForegroundColor = foreTmp;
                 Console.BackgroundColor = backTmp;
             }
