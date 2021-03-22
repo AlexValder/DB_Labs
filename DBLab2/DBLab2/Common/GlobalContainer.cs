308f353e5f97eedb074160271d33f5c79fe940ab
AlexValder <angelaarhangelska@gmail.com>
2021-03-22 23:33:07 +0200

    Sorry Pasha

--- DBLab2/DBLab2/Common/GlobalContainer.cs
+++ DBLab2/DBLab2/Common/GlobalContainer.cs
@@ -1,35 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DBlab2
-{
-    static class GlobalContainer
-    {
-        private static Dictionary<string, string[]> DictOfLists = new Dictionary<string, string[]>
-        {
-            ["Author"] = new string[] { "FirstName", "LastName" },
-            ["Book"] = new string[] { "Title", "Year", "PublisherId", "Publisher" },
-            ["BookAuthor"] = new string[] { "BookId", "AuthorId", "Book", "Author" },
-            ["Cathedra"] = new string[] { "FacultyId", "Name", "Faculty" },
-            ["Faculty"] = new string[] { "Name" },
-            ["Group"] = new string[] { "Number", "SpecialityId", "CathedraId", "Cathedra", "Speciality" },
-            ["LibraryContext"] = new string[] { "Specialities", "Faculties", "Groups", "Cathedras",
-                "Students", "Authors", "Books", "BookAuthors", "Publishers", "StudentCards", "Teachers", "Positions",
-                "TeacherCards", "LibraryEmployees"},
-            ["LibraryEmployee"] = new string[] { "FirstName", "LastName", "PositionId", "Position" },
-            ["Position"] = new string[] { "PosName" },
-            ["Program"] = new string[] { "" },
-            ["Publisher"] = new string[] { "Name" },
-            ["Speciality"] = new string[] { "Name", "Number", "FacultyId", "Faculty" },
-            ["Student"] = new string[] { "FirstName", "LastName", "Year", "GroupId", "Group" },
-            ["StudentCard"] = new string[] { "StudentId", "TakenDate", "DueDate", "ReturnedDate",
-                "BookId", "LibraryEmployeeId", "Student", "Book", "LibraryEmployee" },
-            ["Teacher"] = new string[] { "FirstName", "SecondName", "CathedraId", "Cathedra" },
-            ["TeacherCard"] = new string[] { "TeacherId", "TakenDate", "ReturnedDate", "LibraryEmployeeId",
-                "BookId", "Teacher", "LibraryEmployee", "Book"}
-        };
+namespace DBlab2.Common {
+    static class GlobalContainer {
+        private static readonly ImmutableDictionary<string, string[]> DictOfLists
+            = new Dictionary<string, string[]>(StringComparer.InvariantCultureIgnoreCase) {
+                ["Author"] = new[] {
+                    "FirstName",
+                    "LastName",
+                },
+                ["Book"] = new[] {
+                    "Title",
+                    "Year",
+                    "PublisherId",
+                },
+                ["BookAuthor"] = new[] {
+                    "BookId",
+                    "AuthorId",
+                },
+                ["Cathedra"] = new[] {
+                    "FacultyId",
+                    "Name",
+                },
+                ["Faculty"] = new[] {
+                    "Name",
+                },
+                ["Group"] = new[] {
+                    "Number",
+                    "SpecialityId",
+                    "CathedraId",
+                },
+                ["LibraryEmployee"] = new[] {
+                    "FirstName",
+                    "LastName",
+                    "PositionId",
+                },
+                ["Position"] = new[] {
+                    "PosName",
+                },
+                ["Publisher"] = new[] {
+                    "Name"
+                },
+                ["Speciality"] = new[] {
+                    "Name",
+                    "Number",
+                    "FacultyId",
+                },
+                ["Student"] = new[] {
+                    "FirstName",
+                    "LastName",
+                    "Year",
+                    "GroupId",
+                },
+                ["StudentCard"] = new[] {
+                    "StudentId",
+                    "TakenDate",
+                    "DueDate",
+                    "ReturnedDate",
+                    "BookId",
+                    "LibraryEmployeeId",
+                },
+                ["Teacher"] = new[] {
+                    "FirstName",
+                    "SecondName",
+                    "CathedraId",
+                },
+                ["TeacherCard"] = new[] {
+                    "TeacherId",
+                    "TakenDate",
+                    "ReturnedDate",
+                    "LibraryEmployeeId",
+                    "BookId",
+                },
+            }.ToImmutableDictionary();
+
+        public static bool TableExists(string tableName) => DictOfLists.Keys.Contains(tableName);
+
+        public static bool FieldExists(string tableName, string field)
+            => TableExists(tableName) && DictOfLists[tableName].Contains(field);
     }
 }
