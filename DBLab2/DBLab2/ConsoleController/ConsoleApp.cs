1eea7d78d07a0b40f21ec80fac7417b1e6d27e3f
AlexValder <angelaarhangelska@gmail.com>
2021-03-23 23:30:38 +0200

    Regex is left

--- DBLab2/DBLab2/ConsoleController/ConsoleApp.cs
+++ DBLab2/DBLab2/ConsoleController/ConsoleApp.cs
@@ -5,31 +5,36 @@ using DBLab2.Common;
 
 namespace DBLab2.ConsoleController {
     public static class ConsoleApp {
-        private static readonly SqlParser Parser = new SqlParser();
         private static readonly ImmutableDictionary<string, Action> Actions =
             new Dictionary<string, Action> {
-            ["SELECT"] = () => { Parser.ParseSelect(_input); },
-            ["ADD"]    = () => { Parser.ParseAdd(_input); },
-            ["INSERT"] = () => { Parser.ParseInsert(_input); },
-            ["DELETE"] = () => { Parser.ParseDelete(_input); },
+            ["SELECT"] = () => { SqlParser.ParseSelect(_input); },
+            ["ADD"]    = () => { SqlParser.ParseAdd(_input); },
+            ["INSERT"] = () => { SqlParser.ParseInsert(_input); },
+            ["DELETE"] = () => { SqlParser.ParseDelete(_input); },
             ["Exit"]   = () => { _exit = true; }
         }.ToImmutableDictionary(StringComparer.OrdinalIgnoreCase);
+        
+        private static bool _exit;
+        private static string _input = "";
 
         public static void Run() {
             MainLoop();
         }
-
         
-        private static bool _exit = false;
-        private static string _input = "";
         private static void MainLoop() {
             while (!_exit) {
                 Printer.Info("Enter command: ");
                 _input = Console.ReadLine() ?? string.Empty;
                 if (_input == string.Empty) continue;
 
-                var input = _input[0.._input.IndexOf(' ')] ?? string.Empty;
-                
+                string input;
+                try {
+                    input = _input[0.._input.IndexOf(' ')] ?? string.Empty;
+                }
+                catch {
+                    input = "";
+                }
+
                 if (Actions.ContainsKey(input)) {
                     Actions[input].Invoke();
                 }
--- DBLab2/DBLab2/ConsoleController/SqlParser.cs
+++ DBLab2/DBLab2/ConsoleController/SqlParser.cs
@@ -1,21 +1,51 @@
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using DBLab2.Common;
 
 namespace DBLab2.ConsoleController {
-    class SqlParser {
-        public void ParseSelect(string input) {
-            Printer.Info("In Select: {0}", input);
+    internal static class SqlParser {
+
+        private enum CommandType {
+            Select,
+            Add,
+            Insert,
+            Delete,
         }
 
-        public void ParseAdd(string input) {
-            Printer.Info("In Add: {0}", input);            
-        }
+        private const string SELECT_PATTERN = @"SELECT ";
+        private const string ADD_PATTERN = @"ADD ";
+        private const string INSERT_PATTERN = @"INSERT INTO ";
+        private const string DELETE_PATTERN = @"DELETE ";
 
-        public void ParseInsert(string input) {
-            Printer.Info("In Insert: {0}", input);
-        }
+        private const RegexOptions OPTIONS = RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase;
 
-        public void ParseDelete(string input) {
-            Printer.Info("In Delete: {0}", input);
+        public static void ParseSelect(in string input) => Parse(input, CommandType.Select);
+
+        public static void ParseAdd(in string input) => Parse(input, CommandType.Add);
+
+        public static void ParseInsert(in string input) => Parse(input, CommandType.Insert);
+
+        public static void ParseDelete(in string input) => Parse(input, CommandType.Delete);
+
+        private static void Parse(in string input, in CommandType type) {
+            var regex = new Regex(GetPattern(type), OPTIONS);
+            var match = regex.Match(input);
+            if (match.Success) {
+                Printer.Success("Command is regex-correct: {0}", input);
+            }
+            else {
+                Printer.Error("Command in bullshit: {0}", input);
+            }
+
+            static string GetPattern(in CommandType type) => type switch {
+                CommandType.Select => SELECT_PATTERN,
+                CommandType.Insert => INSERT_PATTERN,
+                CommandType.Add    => ADD_PATTERN,
+                CommandType.Delete => DELETE_PATTERN,
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
         }
     }
 }
