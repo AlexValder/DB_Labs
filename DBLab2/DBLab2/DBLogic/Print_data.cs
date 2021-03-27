fdb2bac3df737c60bc884f81c6d8ec5c592239a0
Pasha <SoloviovPS@gmail.com>
2021-03-27 19:33:23 +0300

    printer but i need to see it in work for debug

---
+++ DBLab2/DBLab2/DBLogic/Print_data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLab2.DBLogic
+{
+    static class Class1
+    {
+        static int size = 25; 
+        public static void PrintData(List<List<string>> Data)
+        {
+            foreach (var f in Data)
+            {
+                Console.Write("|");
+                foreach (var ff in f)
+                {
+                    Console.Write("{0," + size + "}|", ff);
+                }
+                Console.WriteLine();
+                Console.WriteLine(new String('=', (size + 1) * f.Count + 1));
+            }
+        }
+    }
+}
